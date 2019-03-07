e1e94c43a98351566c0764682ea32cec78ec8e04
sisobus <sisobus1@gmail.com>
2019-03-07 13:09:24 +0900

    initialize project with typescript-library-starter

---
+++ test/cornerstone-base64-image-loader.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/cornerstone-base64-image-loader"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
